d506e69bff65d909ed87bbcaef37d275ec9df2a4
Panna Krisztina Pazonyi <ppannaxyz@gmail.com>
2025-04-08 12:11:52 +0200

    Initial commit

---
+++ movie-app/movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Panna Krisztina Pazonyi on 2025. 04. 06..
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
